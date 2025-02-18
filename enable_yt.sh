5ce26b2b4bbae6caf662d46f2097f240495f124b
Sergey <10003391+gnomba@users.noreply.github.com>
2025-02-18 21:34:05 +0300

    modified:   enable_yt.sh

--- enable_yt.sh
+++ enable_yt.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -x
+vCURL="$(which curl) -s"
 
 # youtubeUnblock #
 vNAME="youtubeUnblock"
@@ -16,13 +17,13 @@ echo "${vURL}/${vFILELUCI}"
 opkg install ${vURL}/${vFILE}
 opkg install ${vURL}/${vFILELUCI}
 
-vDOMAINS_URL="https://raw.githubusercontent.com/v2fly/domain-list-community/refs/heads/master/data"; echo "vDOMAINS_URL=${vDOMAINS_URL}"
-vDOMAINS_LIST="youtube discord facebook instagram whatsapp twitter"; echo "vDOMAINS_LIST=${vDOMAINS_LIST}"
+vDOMAINS_URL="https://raw.githubusercontent.com/v2fly/domain-list-community/refs/heads/master/data"
+echo "vDOMAINS_URL=${vDOMAINS_URL}"
+vDOMAINS_LIST="youtube discord facebook instagram whatsapp twitter"
+echo "vDOMAINS_LIST=${vDOMAINS_LIST}"
 vDOMAINS_CUSTOM="googleapis.com play.google.com googleusercontent.com gstatic.com l.google.com gvt1.com yt-video-upload.l.google.com meta.ai meta.com
-filmix.day rutracker.org nnmclub.to upaste.me dmitry-tv.ddns.net dmi3y-tv.ru"; echo "vDOMAINS_CUSTOM=${vDOMAINS_CUSTOM}"
-
-vCURL="$(which curl) -s"
-## TODO vFILTER : grep -v "^#\|^$\|^include\:\|-ads\|ads-" | sed "s/link\://g;s/ @.*$//g;s/full\://g"
+filmix.day rutracker.org nnmclub.to upaste.me dmitry-tv.ddns.net dmi3y-tv.ru"
+echo "vDOMAINS_CUSTOM=${vDOMAINS_CUSTOM}"
 
 uci set youtubeUnblock.@section[0].sni_domains=''
 uci set youtubeUnblock.@section[0].all_domains='0'
@@ -33,7 +34,7 @@ for vYT_ITEM in ${vDOMAINS_CUSTOM}; do
 done
 
 for vDOMAIN_ITEM in ${vDOMAINS_LIST};do
-    echo "###--- vDOMAIN_ITEM ---###"
+    echo "###--- ${vDOMAIN_ITEM} ---###"
     vITEM_URL="${vDOMAINS_URL}/${vDOMAIN_ITEM}"
     for vYT_ITEM in $(${vCURL} ${vITEM_URL} | grep -v "^#\|^$\|^include\:\|-ads\|ads-" | sed "s/link\://g;s/ @.*$//g;s/full\://g"); do
         uci add_list youtubeUnblock.@section[0].sni_domains=${vYT_ITEM}
