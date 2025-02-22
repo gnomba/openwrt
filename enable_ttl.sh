11f42b29de335ceb5d319edab1df474088888594
Sergey <10003391+gnomba@users.noreply.github.com>
2025-02-22 17:44:43 +0300

    modified:   enable_ttl.sh

--- enable_ttl.sh
+++ enable_ttl.sh
@@ -4,26 +4,25 @@
 
 set -x
 
-# exit if routerich #
 vBOARD_ID="$(cat /etc/board.json | grep id | sed 's/\"/ /g;s/,/ /g' | awk '{print $3}')"
+
 if [ "${vBOARD_ID}" == "routerich" ]; then 
-    echo " --- DETECTED BOARD: ${vBOARD_ID} --- Exit with code '1' ---"
-    exit 1
+    echo " --- DETECTED BOARD: routerich ---"
+else
+    vNAME="ttl"
+    vVERSION="0.0.3"
+    vFILELUCI="luci-app-${vNAME}_${vVERSION}_all.ipk"
+    vFILELUCILANG="luci-i18n-${vNAME}-ru_git-24.255.44995-eddd63f_all.ipk"
+    vURL="https://github.com/routerich/packages.routerich/raw/refs/heads/23.05.5/routerich"
+
+    wget ${vURL}/${vFILELUCI} -O /tmp/${vFILELUCI}
+    wget ${vURL}/${vFILELUCILANG} -O /tmp/${vFILELUCILANG}
+
+    opkg install /tmp/${vFILELUCI}
+    opkg install /tmp/${vFILELUCILANG}
+
+    rm -fv /tmp/*${vNAME}*
 fi
-
-vNAME="ttl"
-vVERSION="0.0.3"
-vFILELUCI="luci-app-${vNAME}_${vVERSION}_all.ipk"
-vFILELUCILANG="luci-i18n-${vNAME}-ru_git-24.255.44995-eddd63f_all.ipk"
-vURL="https://github.com/routerich/packages.routerich/raw/refs/heads/23.05.5/routerich"
-
-wget ${vURL}/${vFILELUCI} -O /tmp/${vFILELUCI}
-wget ${vURL}/${vFILELUCILANG} -O /tmp/${vFILELUCILANG}
-
-opkg install /tmp/${vFILELUCI}
-opkg install /tmp/${vFILELUCILANG}
-
-rm -fv /tmp/*${vNAME}*
 
 set +x
 
