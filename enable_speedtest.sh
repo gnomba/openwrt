be69524e16d6e3799fd610954cff380a61b95b96
Sergey <10003391+gnomba@users.noreply.github.com>
2025-02-22 16:54:50 +0300

    modified:   enable_speedtest.sh

--- enable_speedtest.sh
+++ enable_speedtest.sh
@@ -3,7 +3,9 @@
 
 set -x
 
-curl -s https://raw.githubusercontent.com/animegasan/luci-app-speedtest/master/install | sh
+vNAME="speedtest"
+
+curl -s https://raw.githubusercontent.com/animegasan/luci-app-${vNAME}/master/install | sh
 
 set +x
 
