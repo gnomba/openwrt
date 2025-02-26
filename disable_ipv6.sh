284dd13ca9284d93a16a1237d4f250b3301a8ca5
Sergey <10003391+gnomba@users.noreply.github.com>
2025-02-26 22:48:23 +0300

    modified:   disable_ipv6.sh

--- disable_ipv6.sh
+++ disable_ipv6.sh
@@ -30,7 +30,7 @@ echo -e "    net.ipv6.conf.all.disable_ipv6=1 \\" >> ${vSERVICE_FILE}
 echo -e "    net.ipv6.conf.default.disable_ipv6=1 \\" >> ${vSERVICE_FILE}
 echo -e "    net.ipv6.conf.lo.disable_ipv6=1" >> ${vSERVICE_FILE}
 echo -e "}\n" >> ${vSERVICE_FILE}
-echo "exit 0" >> ${vSERVICE_FILE}
+#echo "exit 0" >> ${vSERVICE_FILE}
 
 chmod +x ${vSERVICE_FILE}
 
