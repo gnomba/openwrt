71843a890d8b2c385edc7eaa528030af7588f3aa
Sergey <10003391+gnomba@users.noreply.github.com>
2025-02-26 22:18:46 +0300

    modified:   disable_ipv6.sh

--- disable_ipv6.sh
+++ disable_ipv6.sh
@@ -3,21 +3,24 @@
 
 set -x
 
-uci set network.lan.ipv6='0'
-uci set network.wan.ipv6='0'
-uci set dhcp.lan.dhcpv6='disabled'
-uci -q delete dhcp.lan.dhcpv6
-uci -q delete dhcp.lan.ra
-uci set network.lan.delegate='0'
-uci -q delete network.globals.ula_prefix
-uci delete network.wan6
-/etc/init.d/odhcpd disable
-/etc/init.d/odhcpd stop
-uci commit
-/etc/init.d/network restart
-sysctl -w net.ipv6.conf.all.disable_ipv6=1
+#uci set network.lan.ipv6='0'
+#uci set network.wan.ipv6='0'
+#uci set dhcp.lan.dhcpv6='disabled'
+#uci -q delete dhcp.lan.dhcpv6
+#uci -q delete dhcp.lan.ra
+#uci set network.lan.delegate='0'
+#uci -q delete network.globals.ula_prefix
+#uci delete network.wan6
+#/etc/init.d/odhcpd disable
+#/etc/init.d/odhcpd stop
+#uci commit
+#/etc/init.d/network restart
+
 echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
-sysctl -w net.ipv6.conf.default.disable_ipv6=1
-sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+
+sysctl -wq \
+net.ipv6.conf.all.disable_ipv6=1 \
+net.ipv6.conf.default.disable_ipv6=1 \
+net.ipv6.conf.lo.disable_ipv6=1
 
 exit 0
