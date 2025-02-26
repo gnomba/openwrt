92e7b1152ccf4b8911055a07206606f6d7d05594
Sergey <10003391+gnomba@users.noreply.github.com>
2025-02-26 22:31:14 +0300

    modified:   disable_ipv6.sh

--- disable_ipv6.sh
+++ disable_ipv6.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 # INFO: https://lsetc.ru/openwrt-otkljuchit-ipv6/
-
-set -x
-
 #uci set network.lan.ipv6='0'
 #uci set network.wan.ipv6='0'
 #uci set dhcp.lan.dhcpv6='disabled'
@@ -16,11 +13,27 @@ set -x
 #uci commit
 #/etc/init.d/network restart
 
+set -x
+
+uci delete network.wan6
+uci commit network
+
 echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
 
-sysctl -wq \
-net.ipv6.conf.all.disable_ipv6=1 \
-net.ipv6.conf.default.disable_ipv6=1 \
-net.ipv6.conf.lo.disable_ipv6=1
+vSERVICE_FILE="/etc/init.d/disable_ipv6"
+
+echo -e "#!/bin/sh /etc/rc.common\n\n" > ${vSERVICE_FILE}
+echo -e "START=11\n\n" >> ${vSERVICE_FILE}
+echo -e "start() {\n" >> ${vSERVICE_FILE}
+echo -e "    sysctl -wq \\\n" >> ${vSERVICE_FILE}
+echo -e "    net.ipv6.conf.all.disable_ipv6=1 \\\n" >> ${vSERVICE_FILE}
+echo -e "    net.ipv6.conf.default.disable_ipv6=1 \\n" >> ${vSERVICE_FILE}
+echo -e "    net.ipv6.conf.lo.disable_ipv6=1\n" >> ${vSERVICE_FILE}
+echo -e "}\n\n" >> ${vSERVICE_FILE}
+echo "exit 0" >> ${vSERVICE_FILE}
+
+chmod +x ${vSERVICE_FILE}
+
+/etc/init.d/network restart
 
 exit 0
