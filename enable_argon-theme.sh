47eaabc4c13be54a1d90979a989af8a62e421b19
Sergey <10003391+gnomba@users.noreply.github.com>
2025-02-28 21:59:21 +0300

    modified:   README.md modified:   enable_argon-theme.sh

--- enable_argon-theme.sh
+++ enable_argon-theme.sh
@@ -7,7 +7,7 @@ vARGON_CFG="luci-app-argon-config"
 vRR_THM="luci-theme-routerich"
 
 vRR_THM_CHK="$(opkg list-installed | grep "^${vRR_THM}" | awk '{print $1}' | wc -l)"
-if [ "${vRRTHEME_CHK}" -eq "1" ]; then
+if [ "${vRR_THM_CHK}" -eq "1" ]; then
     opkg remove ${vRR_THM}
 fi
 
