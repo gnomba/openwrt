04e6ffe1a093ae1b38f59b986ff2c3dfa9c45176
Sergey <10003391+gnomba@users.noreply.github.com>
2025-03-03 21:38:24 +0300

    modified:   Xiaomi_MI-R3/openwrt-23.05.5-manual-setup.sh modified:   disable_ipv6.sh

--- Xiaomi_MI-R3/openwrt-23.05.5-manual-setup.sh
+++ Xiaomi_MI-R3/openwrt-23.05.5-manual-setup.sh
@@ -5,7 +5,6 @@ set -x
 
 #opkg remove --force-depends dnsmasq luci-proto-ipv6 odhcp6c odhcpd-ipv6only 
 opkg update
-opkg install dnsmasq-full libnettle8 libgmp10 libnetfilter-conntrack3 libnfnetlink0 kmod-nf-conntrack-netlink kmod-nf-conntrack6 kmod-nf-log6 kmod-nf-reject6
 opkg install curl libcurl4 libnghttp2-14
 opkg install wget-ssl libpcre2 zlib libopenssl3 libatomic1 librt
 opkg install zram-swap kmod-zram
@@ -14,7 +13,12 @@ opkg install adblock luci-app-adblock luci-i18n-adblock-ru coreutils coreutils-s
 opkg install https-dns-proxy luci-app-https-dns-proxy luci-i18n-https-dns-proxy-ru libcares libev resolveip
 opkg install ddns-scripts ddns-scripts-services luci-app-ddns luci-i18n-ddns-ru
 opkg install ttyd luci-app-ttyd libcap libuv1 libwebsockets-full
-opkg install igmpproxy htop ttyd
+opkg install igmpproxy htop ttyd mc nano-full vim-full diffutils
+
+#opkg remove dnsmasq
+#echo 'nameserver 1.1.1.1' >> /etc/resolv.conf
+#opkg update
+#opkg install dnsmasq-full libnettle8 libgmp10 libnetfilter-conntrack3 libnfnetlink0 kmod-nf-conntrack-netlink kmod-nf-conntrack6 kmod-nf-log6 kmod-nf-reject6
 
 curl -s https://raw.githubusercontent.com/gnomba/openwrt/refs/heads/main/enable_fantastic-packages.sh | sh
 curl -s https://raw.githubusercontent.com/gnomba/openwrt/refs/heads/main/enable_argon-theme.sh | sh
--- disable_ipv6.sh
+++ disable_ipv6.sh
@@ -3,18 +3,18 @@
 
 set -x
 
-uci set network.lan.ipv6='0'
-uci set network.wan.ipv6='0'
-uci set dhcp.lan.dhcpv6='disabled'
-uci -q delete dhcp.lan.dhcpv6
-uci -q delete dhcp.lan.ra
-uci set network.lan.delegate='0'
+#uci set network.lan.ipv6='0'
+#uci set network.wan.ipv6='0'
+#uci set dhcp.lan.dhcpv6='disabled'
+#uci -q delete dhcp.lan.dhcpv6
+#uci -q delete dhcp.lan.ra
+#uci set network.lan.delegate='0'
 #uci -q delete network.globals.ula_prefix
-uci delete network.wan6
-/etc/init.d/odhcpd disable
-/etc/init.d/odhcpd stop
-uci commit
-/etc/init.d/network restart
+#uci delete network.wan6
+#/etc/init.d/odhcpd disable
+#/etc/init.d/odhcpd stop
+#uci commit network
+#/etc/init.d/network restart
 
 vNAME="disable_ipv6"
 vSERVICE_FILE="/etc/init.d/${vNAME}"
