993e74de51b4eed96aea5b8cbae1c3949c6393cc
Sergey <10003391+gnomba@users.noreply.github.com>
2025-03-08 19:39:32 +0300

    modified:   Xiaomi_MI-R3/openwrt-23.05.5-manual-setup.sh

--- Xiaomi_MI-R3/openwrt-23.05.5-manual-setup.sh
+++ Xiaomi_MI-R3/openwrt-23.05.5-manual-setup.sh
@@ -21,8 +21,8 @@ curl -s https://openwrt.132lan.ru/packages/${vBRANCH}/packages/add.sh | sh
 vMODEM="adb adb-enablemodem atinout comgt comgt-ncm fm350-modem kmod-usb-acm kmod-usb-core kmod-usb-net kmod-usb-net-cdc-ether kmod-usb-net-cdc-mbim kmod-usb-net-cdc-ncm kmod-usb-net-huawei-cdc-ncm
 kmod-usb-net-qmi-wwan kmod-usb-net-rndis kmod-usb-serial kmod-usb-serial-option kmod-usb-serial-qualcomm kmod-usb-serial-wwan kmod-usb-wdm libmbim libqmi luci-app-atinout luci-app-modeminfo
 luci-app-smstools3 luci-i18n-atinout-ru luci-i18n-modeminfo-ru luci-i18n-smstools3-ru luci-proto-3g luci-proto-fm350 luci-proto-mbim luci-proto-ncm luci-proto-ppp luci-proto-qmi luci-proto-xmm
-modeminfo modeminfo-qmi modeminfo-serial-dell modeminfo-serial-fibocom modeminfo-serial-gosun modeminfo-serial-huawei modeminfo-serial-meig modeminfo-serial-mikrotik modeminfo-serial-quectel
-modeminfo-serial-sierra modeminfo-serial-simcom modeminfo-serial-styx modeminfo-serial-telit modeminfo-serial-thales modeminfo-serial-tw modeminfo-serial-xmm modeminfo-serial-yuge modeminfo-serial-zte
+modeminfo modeminfo-qmi modeminfo-serial-fm350 modeminfo-serial-dell modeminfo-serial-fibocom modeminfo-serial-gosun modeminfo-serial-huawei modeminfo-serial-meig modeminfo-serial-mikrotik modeminfo-serial-quectel
+modeminfo-serial-simcom-a7xxx modeminfo-serial-sierra modeminfo-serial-simcom modeminfo-serial-styx modeminfo-serial-telit modeminfo-serial-thales modeminfo-serial-tw modeminfo-serial-xmm modeminfo-serial-yuge modeminfo-serial-zte
 ppp qmi-utils sms-tool smstools3 umbim uqmi wwan xmm-modem"
 for vITEM_MODEM in ${vMODEM}; do
     opkg install ${vITEM_MODEM}
