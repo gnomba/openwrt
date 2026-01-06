f5c7f09b2c64f8b086b4c9292ce4f47d7d3d65f3
Sergey <10003391+gnomba@users.noreply.github.com>
2026-01-06 19:43:35 +0300

    modified:   Mercusys_MR70X_v1/build_24.10.4_openwrt.txt modified:   RouteRich_AX3000/build_24.10.4_openwrt.txt modified:   RouteRich_AX3000_v1/build_24.10.4_openwrt.txt modified:   Xiaomi_AX3000T/build_24.10.5_openwrt.txt modified:   Xiaomi_AX3000T/build_25.12.0-rc1_openwrt.txt modified:   enable_fantastic-packages.sh

--- enable_fantastic-packages.sh
+++ enable_fantastic-packages.sh
@@ -3,11 +3,21 @@
 
 set -x
 
-vNAME="fantastic"
-vBASE_URL="https://${vNAME}-packages.github.io/packages/releases"
+opkg update || apk update
+opkg install --force-checksum jq  || apk add --allow-untrusted jq
 
-vALLOW_VER="("23.05" "24.10" "SNAPSHOT")"
-vALLOW_ARCH="("aarch64_cortex-a53" "aarch64_generic" "arm_cortex-a15_neon-vfpv4" "mips_24kc" "mipsel_24kc" "x86_64")"
+vRed='\033[1;31m'
+vGreen='\033[1;32m'
+vYellow='\033[1;33m'
+vWhite='\033[1;37m'
+vColor_Off='\033[0m'
+
+vNAME="fantastic"
+. /etc/openwrt_release
+vOWRT_VER="${DISTRIB_RELEASE%.*}"
+vARCH="${DISTRIB_ARCH}"
+vWGET_CMD="wget -q --show-progress -c"
+
 vALLOW_PKGS="luci-app-cpu-perf luci-i18n-cpu-perf-ru
 luci-app-cpu-status luci-i18n-cpu-status-ru
 internet-detector luci-app-internet-detector luci-i18n-internet-detector-ru
@@ -18,67 +28,83 @@ luci-app-temp-status luci-i18n-temp-status-ru
 luci-app-change-mac
 "
 
-sed -i "s/-00/\.0/g" /etc/openwrt_release; cat /etc/openwrt_release
-. /etc/openwrt_release
-vOWRT_VER="${DISTRIB_RELEASE%.*}"
-vARCH="${DISTRIB_ARCH}"
-vWGET_CMD="wget -q --show-progress -c"
-
-if ! [[ "$vALLOW_VER[@]" =~ "$vOWRT_VER" ]]; then
-    echo " ###"
-    echo " ### NEED OpenWrt VERSION -> $vALLOW_VER ###"
-    echo " ###"
-else
-    if ! [[ "$vALLOW_ARCH[@]" =~ "$vARCH" ]]; then
-        echo " ###"
-        echo " ### NEED arch -> $vALLOW_ARCH ###"
-        echo " ###"
-    else
-        if [ "$(opkg list-installed | grep "${vNAME}" | wc -l)" -lt "2" ]; then
-            vKEYRING_FILE="$(curl -s ${vBASE_URL}/${vOWRT_VER}/packages/${vARCH}/packages/ | grep ${vNAME}-keyring | sed "s/\"/ /g" | awk '{print $5}')"
-            vFEEDS_FILE="$(curl -s ${vBASE_URL}/${vOWRT_VER}/packages/${vARCH}/packages/ | grep ${vNAME}-packages-feeds | sed "s/\"/ /g" | awk '{print $5}')"
-            ${vWGET_CMD} ${vBASE_URL}/${vOWRT_VER}/packages/${vARCH}/packages/${vKEYRING_FILE} -O /tmp/${vKEYRING_FILE}
-            ${vWGET_CMD} ${vBASE_URL}/${vOWRT_VER}/packages/${vARCH}/packages/${vFEEDS_FILE} -O /tmp/${vFEEDS_FILE}
-            opkg install /tmp/${vKEYRING_FILE}
-            opkg install /tmp/${vFEEDS_FILE}
-            rm -fv /tmp/*${vNAME}*
-        else
-            echo " ###"
-            echo " ### ${vNAME}-packages repository already added ###"
-            echo " ###"
-        fi
-
-        opkg update
-        
-        for vPKG in ${vALLOW_PKGS};do
-            if [ "$(opkg list-installed | grep "^${vPKG}" | wc -l)" -eq "1" ]; then
-                echo " ###"
-                echo " ### ${vPKG} already installed ###"
-                echo " ###"
-            else
-                opkg install ${vPKG}
-            fi
-        done
-        
-        /etc/init.d/rpcd reload
-
-        uci set cpu-perf.config.enabled='0'
-        /etc/init.d/cpu-perf disable
-        /etc/init.d/cpu-perf stop
-        uci commit cpu-perf
-
-        uci set internet-detector.internet.mod_public_ip_provider='google'
-        /etc/init.d/internet-detector enable
-        /etc/init.d/internet-detector start
-        uci commit internet-detector
-
-        uci set change-mac.@change-mac[0].mac_type_specific='24:0F:5E'
-        uci set change-mac.@change-mac[0].mac_type_vendor='router:zrouterTechn'
-        /etc/init.d/change-mac disable
-        /etc/init.d/change-mac stop
-        uci commit change-mac
+case ${vOWRT_VER} in
+    23.05)
+    vBASE_URL="https://github.com/${vNAME}-packages/releases/tree/archive/${vOWRT_VER}/packages"
+    vALLOW_ARCH="("aarch64_cortex-a53" "aarch64_cortex-a72" "aarch64_generic" "arm_cortex-a15_neon-vfpv4" "arm_cortex-a9_vfpv3-d16" "mips_24kc" "mipsel_24kc" "riscv64_riscv64" "x86_64")"
+    echo -e " ${vGreen}[+] ${vWhite}Version: ${vGreen}${vOWRT_VER} ${vARCH} ${vYellow}\n     continue ...${vColor_Off}"
+    vKEYRING_FILE="$(curl -s ${vBASE_URL}/${vARCH}/packages/ | grep packages/${vNAME}-keyring | sed -e 's/<[^>]*>//g' | jq | grep ${vNAME}-keyring | awk -F\" '{print $4}' | head -n 1)"
+    vFEEDS_FILE="$(curl -s ${vBASE_URL}/${vARCH}/packages/ | grep packages/${vNAME}-packages-feeds | sed -e 's/<[^>]*>//g' | jq | grep ${vNAME}-packages-feeds | awk -F\" '{print $4}' | head -n 1)"
+    ${vWGET_CMD} ${vBASE_URL}/${vARCH}/packages/${vKEYRING_FILE} -O /tmp/${vKEYRING_FILE}
+    ${vWGET_CMD} ${vBASE_URL}/${vARCH}/packages/${vFEEDS_FILE} -O /tmp/${vFEEDS_FILE}
+    if [ "$(opkg list-installed | grep "${vNAME}" | wc -l)" -lt "2" ]; then
+        opkg install /tmp/${vKEYRING_FILE}
+        opkg install /tmp/${vFEEDS_FILE}
     fi
-fi
+    opkg update
+    for vPKG in ${vALLOW_PKGS};do
+        opkg install ${vPKG}
+    done
+    ;;
+    24.10)
+    vBASE_URL="https://${vNAME}-packages.github.io/releases"
+    vALLOW_ARCH="("aarch64_cortex-a53" "aarch64_cortex-a72" "aarch64_generic" "arm_cortex-a15_neon-vfpv4" "arm_cortex-a9_vfpv3-d16" "mips_24kc" "mipsel_24kc" "riscv64_riscv64" "x86_64")"
+    echo -e " ${vGreen}[+] ${vWhite}Version: ${vGreen}${vOWRT_VER} ${vARCH} ${vYellow}\n     continue ...${vColor_Off}"
+    vKEYRING_FILE="$(curl -s ${vBASE_URL}/${vOWRT_VER}/packages/${vARCH}/packages/ | grep ${vNAME}-keyring | sed "s/\"/ /g" | awk '{print $5}')"
+    vFEEDS_FILE="$(curl -s ${vBASE_URL}/${vOWRT_VER}/packages/${vARCH}/packages/ | grep ${vNAME}-packages-feeds | sed "s/\"/ /g" | awk '{print $5}')"
+    ${vWGET_CMD} ${vBASE_URL}/${vOWRT_VER}/packages/${vARCH}/packages/${vKEYRING_FILE} -O /tmp/${vKEYRING_FILE}
+    ${vWGET_CMD} ${vBASE_URL}/${vOWRT_VER}/packages/${vARCH}/packages/${vFEEDS_FILE} -O /tmp/${vFEEDS_FILE}
+    if [ "$(opkg list-installed | grep "${vNAME}" | wc -l)" -lt "2" ]; then
+        opkg install /tmp/${vKEYRING_FILE}
+        opkg install /tmp/${vFEEDS_FILE}
+    fi
+    opkg update
+    for vPKG in ${vALLOW_PKGS};do
+        opkg install ${vPKG}
+    done
+    ;;
+    25.12)
+    vBASE_URL="https://${vNAME}-packages.github.io/releases"
+    vALLOW_ARCH="("aarch64_cortex-a53" "aarch64_cortex-a72" "aarch64_generic" "arm_cortex-a15_neon-vfpv4" "arm_cortex-a9_vfpv3-d16" "mips_24kc" "mipsel_24kc" "riscv64_generic" "x86_64")"
+    echo -e " ${vGreen}[+] ${vWhite}Version: ${vGreen}${vOWRT_VER} ${vARCH} ${vYellow}\n     continue ...${vColor_Off}"
+    vKEYRING_FILE="$(curl -s ${vBASE_URL}/${vOWRT_VER}/packages/${vARCH}/packages/ | grep ${vNAME}-keyring | sed "s/\"/ /g" | awk '{print $5}')"
+    vFEEDS_FILE="$(curl -s ${vBASE_URL}/${vOWRT_VER}/packages/${vARCH}/packages/ | grep ${vNAME}-packages-feeds | sed "s/\"/ /g" | awk '{print $5}')"
+    ${vWGET_CMD} ${vBASE_URL}/${vOWRT_VER}/packages/${vARCH}/packages/${vKEYRING_FILE} -O /tmp/${vKEYRING_FILE}
+    ${vWGET_CMD} ${vBASE_URL}/${vOWRT_VER}/packages/${vARCH}/packages/${vFEEDS_FILE} -O /tmp/${vFEEDS_FILE}
+    if [ "$(apk list -a ${vNAME}* | grep "${vNAME}" | wc -l)" -lt "2" ]; then
+        apk add --allow-untrusted /tmp/${vKEYRING_FILE}
+        apk add --allow-untrusted /tmp/${vFEEDS_FILE}
+    fi
+    apk update
+    for vPKG in ${vALLOW_PKGS};do
+        apk add ${vPKG}
+    done
+    ;;
+    *)
+    echo -e " ${vRed}[-] ${vWhite}Unknown version: ${vRed}${vOWRT_VER} ${vARCH} ${vYellow}\n     exit 1 ...${vColor_Off}"
+    exit 1
+    ;;
+esac
+
+rm -fv /tmp/*${vNAME}*
+
+/etc/init.d/rpcd reload
+
+uci set cpu-perf.config.enabled='0'
+/etc/init.d/cpu-perf disable
+/etc/init.d/cpu-perf stop
+uci commit cpu-perf
+
+uci set internet-detector.internet.mod_public_ip_provider='google'
+/etc/init.d/internet-detector enable
+/etc/init.d/internet-detector start
+uci commit internet-detector
+
+uci set change-mac.@change-mac[0].mac_type_specific='24:0F:5E'
+uci set change-mac.@change-mac[0].mac_type_vendor='router:zrouterTechn'
+/etc/init.d/change-mac disable
+/etc/init.d/change-mac stop
+uci commit change-mac
 
 uci commit
 
