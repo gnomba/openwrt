72fb31590d88004551e06629fda5af8b4877b220
Sergey <10003391+gnomba@users.noreply.github.com>
2026-01-07 16:19:09 +0300

    modified:   enable_argon-theme.sh

--- enable_argon-theme.sh
+++ enable_argon-theme.sh
@@ -26,15 +26,19 @@ vTMP_FILE="/tmp/tmp.zip"
 case ${vOWRT_VER} in
     23.05)
     echo -e " ${vGreen}[+] ${vWhite}Version: ${vGreen}${vOWRT_VER} ${vARCH} ${vYellow}\n     continue ...${vColor_Off}"
-    opkg install https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.3.2/luci-theme-argon_2.3.2-r20250207_all.ipk
-    touch /etc/uci-defaults/luci-argon-config
-    opkg install https://github.com/jerrykuku/luci-app-argon-config/releases/download/v0.9/luci-app-argon-config_0.9_all.ipk
+    opkg install https://github.com/fantastic-packages/releases/raw/refs/heads/archive/23.05/packages/aarch64_cortex-a53/luci/luci-theme-argon_git-24.354.21394-1a22c2e_all.ipk
+    #opkg install https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.3.2/luci-theme-argon_2.3.2-r20250207_all.ipk
+    #touch /etc/uci-defaults/luci-argon-config
+    opkg install https://github.com/fantastic-packages/releases/raw/refs/heads/archive/23.05/packages/aarch64_cortex-a53/luci/luci-app-argon-config_1.0_all.ipk
+    #opkg install https://github.com/jerrykuku/luci-app-argon-config/releases/download/v0.9/luci-app-argon-config_0.9_all.ipk
     ;;
     24.10)
     echo -e " ${vGreen}[+] ${vWhite}Version: ${vGreen}${vOWRT_VER} ${vARCH} ${vYellow}\n     continue ...${vColor_Off}"
-    opkg install https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.3.2/luci-theme-argon_2.3.2-r20250207_all.ipk
-    touch /etc/uci-defaults/luci-argon-config
-    opkg install https://github.com/jerrykuku/luci-app-argon-config/releases/download/v0.9/luci-app-argon-config_0.9_all.ipk
+    opkg install https://github.com/fantastic-packages/releases/raw/refs/heads/archive/23.05/packages/aarch64_cortex-a53/luci/luci-theme-argon_git-24.354.21394-1a22c2e_all.ipk
+    #opkg install https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.3.2/luci-theme-argon_2.3.2-r20250207_all.ipk
+    #touch /etc/uci-defaults/luci-argon-config
+    opkg install https://github.com/fantastic-packages/releases/raw/refs/heads/archive/23.05/packages/aarch64_cortex-a53/luci/luci-app-argon-config_1.0_all.ipk
+    #opkg install https://github.com/jerrykuku/luci-app-argon-config/releases/download/v0.9/luci-app-argon-config_0.9_all.ipk
     ;;
     25.12)
     echo -e " ${vGreen}[+] ${vWhite}Version: ${vGreen}${vOWRT_VER} ${vARCH} ${vYellow}\n     continue ...${vColor_Off}"
