cb6b4111b1af4c840aff938b68c5dcaf0354fb31
Sergey <10003391+gnomba@users.noreply.github.com>
2026-03-21 22:34:14 +0300

    new file:   Redmi_AC2100/uci-defaults modified:   enable_opera-proxy.sh

--- enable_opera-proxy.sh
+++ enable_opera-proxy.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
-# INFO: https://github.com/Snawoot/opera-proxy/releases
+# INFO: https://github.com/gnomba/openwrt/tree/main/_packages
 
 set -x
 
+. /etc/os-release
 vNAME="opera-proxy"
-vARCH="arm64" # amd64,arm,arm64,mips,mips64,mips64le,mipsle
-vVER="$(curl -fs -o/dev/null -w %{redirect_url} https://github.com/Snawoot/${vNAME}/releases/latest | cut -b 54-)"
-vPROXY_IP="$(uci get network.lan.ipaddr)"
+vARCH="$OPENWRT_ARCH"
+vVER="v1.15.0"
+vPROXY_IP="127.0.0.1"
 vPROXY_PORT="18080"
-vPROXY_DNS="https://9.9.9.9/dns-query"
-vPROXY_COUNTRY="EU" # AM,Americas : EU,Europe : AS,Asia
+vPROXY_DNS="https://8.8.4.4/dns-query"
+#vPROXY_COUNTRY="EU" # AM,Americas : EU,Europe : AS,Asia # -country ${vPROXY_COUNTRY}
 vPATH="/usr/bin/${vNAME}"
-vURL="https://github.com/Snawoot/${vNAME}/releases/download/v${vVER}"
-vFILE="${vNAME}.linux-${vARCH}"
+vURL="https://github.com/gnomba/openwrt/raw/refs/heads/main/_packages/${vARCH}"
+vFILE="${vNAME}_${vVER}"
 vOPKG_FILE="/etc/opkg.conf"
 vCURL_FILE="~/.curlrc"
 vWGET_FILE="~/.wgetrc"
@@ -23,9 +24,9 @@ vWGET_SET="#use_proxy = on
 #http_proxy = http://${vPROXY_IP}:${vPROXY_PORT}
 #https_proxy = http://${vPROXY_IP}:${vPROXY_PORT}" 
 
-echo "${vOPKG_SET}" >> ${vOPKG_FILE}
-echo "${vCURL_SET}" >> ${vCURL_FILE}
-echo "${vWGET_SET}" >> ${vWGET_FILE}
+grep -q "proxy" ${vOPKG_FILE} || echo "${vOPKG_SET}" >> ${vOPKG_FILE}
+grep -q "proxy" ${vCURL_FILE} || echo "${vCURL_SET}" >> ${vCURL_FILE}
+grep -q "proxy" ${vWGET_FILE} || echo "${vWGET_SET}" >> ${vWGET_FILE}
 
 service ${vNAME} stop
 echo " ###########################"
@@ -46,7 +47,7 @@ STOP=89
 PROG=${vPATH}
 start_service() {
         procd_open_instance
-        procd_set_param command "\$PROG" -country ${vPROXY_COUNTRY} -bootstrap-dns ${vPROXY_DNS} -bind-address ${vPROXY_IP}:${vPROXY_PORT} -verbosity 50
+        procd_set_param command "\$PROG" -bootstrap-dns ${vPROXY_DNS} -bind-address ${vPROXY_IP}:${vPROXY_PORT} -verbosity 50
         procd_set_param stdout 1
         procd_set_param stderr 1
         procd_set_param respawn \${respawn_threshold:-3600} \${respawn_timeout:-5} \${respawn_retry:-5}
