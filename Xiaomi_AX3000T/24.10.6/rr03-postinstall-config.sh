897c4d0ca403c6f561915295a58a727b185efd24
Sergey <10003391+gnomba@users.noreply.github.com>
2026-09-18 20:30:57 +0300

    modified:   Xiaomi_AX3000T/24.10.6/rr03-postinstall-config.sh deleted:    _zb/luci-app-zeroblock_0.8.5-r26_all.ipk new file:   _zb/luci-app-zeroblock_0.8.5-r31_all.ipk deleted:    _zb/zeroblock_0.8.5-r26_aarch64_cortex-a53.ipk new file:   _zb/zeroblock_0.8.5-r31_aarch64_cortex-a53.ipk

--- Xiaomi_AX3000T/24.10.6/rr03-postinstall-config.sh
+++ Xiaomi_AX3000T/24.10.6/rr03-postinstall-config.sh
@@ -324,7 +324,7 @@ set_ZB() {
 	opkg install --force-checksum --no-check-certificate "https://raw.githubusercontent.com/gnomba/openwrt/refs/heads/main/_zb/xray-core_${XRAY_VER}_aarch64_cortex-a53.ipk"
 	SB_RR_VER="1.14.0-rr-edition-2.7.0-r1"
 	opkg install --force-checksum --no-check-certificate "https://raw.githubusercontent.com/gnomba/openwrt/refs/heads/main/_zb/sing-box-rr_${SB_RR_VER}_aarch64_cortex-a53.ipk"
-	ZB_VER="0.8.5-r26"
+	ZB_VER="0.8.5-r31"
 	opkg install --force-checksum --no-check-certificate "https://raw.githubusercontent.com/gnomba/openwrt/refs/heads/main/_zb/zeroblock_${ZB_VER}_aarch64_cortex-a53.ipk"
 	opkg install --force-checksum --no-check-certificate "https://raw.githubusercontent.com/gnomba/openwrt/refs/heads/main/_zb/luci-app-zeroblock_${ZB_VER}_all.ipk"
 	is_rr && ZB_TG="0.8.5-r8"; opkg install --force-checksum --no-check-certificate "https://raw.githubusercontent.com/gnomba/openwrt/refs/heads/main/_zb/zeroblock-tg_${ZB_TG}_aarch64_cortex-a53.ipk"
